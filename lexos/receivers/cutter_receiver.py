7dc7473edfc304ec45bc354d574432bf2a4550ea
Weiqi97 <weltch1997@gmail.com>
2018-09-28 00:45:43 -0400

    update receiver

--- lexos/receivers/cutter_receiver.py
+++ lexos/receivers/cutter_receiver.py
@@ -1,6 +1,6 @@
 """This is the receiver for the cutter model."""
 
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, List
 
 
 class CutterFrontEndOptions(NamedTuple):
@@ -10,3 +10,5 @@ class CutterFrontEndOptions(NamedTuple):
     last_proportion: float
     # A milestone, it is none if it is not given from frontend.
     milestone: Optional[str]
+    # This is the list of active file ids.
+    active_file_ids: List[int]
