f6319ebf7faa7cb8228575836dcbafbbd5f18d2c
Weiqi97 <weltch1997@gmail.com>
2018-09-28 01:16:35 -0400

    Complete documenting the receiver

--- lexos/receivers/cutter_receiver.py
+++ lexos/receivers/cutter_receiver.py
@@ -6,27 +6,39 @@ from lexos.receivers.base_receiver import BaseReceiver
 
 
 class CutterFrontEndOptions(NamedTuple):
+    """The typed tuple to hold cutter front end option."""
+
+    # The desired segment size of each chunk.
     cut_size: int
+
+    # The desired cutting type.
     cut_type: str
+
+    # The amount of overlapping content at segment boundaries.
     overlap_size: float
+
+    # The smallest size the last segment has to be to become a single chunk.
     last_proportion: float
-    # A milestone, it is none if it is not given from frontend.
+
+    # A milestone to cut by, it is none if it is not given from frontend.
     milestone: Optional[str]
+
     # This is the list of active file ids.
     active_file_ids: List[int]
 
 
 class CutterReceiver(BaseReceiver):
-    """This is the class that gets front end options for the stats model."""
+    """This is the class that gets front end options for the cutter model."""
 
     def __init__(self):
-        """Get stats front end options using the receiver."""
+        """Get cutter front end options using the receiver."""
         super().__init__()
 
     def options_from_front_end(self) -> CutterFrontEndOptions:
         """Get the options from front end.
 
-        The only option is selected file ids.
+        :return: a CutterFrontEndOptions object that contains all desired
+        front end options for the cutter model.
         """
         # Get active file ids from front end as a string.
         active_file_ids_string = self._front_end_data["active_file_ids"]
